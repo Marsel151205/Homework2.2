90524ef3c37b198ea9081993050683b1b9e7c32c
Marsel151205 <mambetalievmarsel2289@gmail.com>
2022-03-18 12:33:35 +0300

    Age

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,19 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(android(35, 20));
-        System.out.println(android(28, 10));
-        System.out.println(android(16, -8));
-        System.out.println(android(16, 30));
-        System.out.println(android(40, 4));
+        System.out.println(android(generateRandomAge(), 20));
+        System.out.println(android(generateRandomAge(), 10));
+        System.out.println(android(generateRandomAge(), -8));
+        System.out.println(android(generateRandomAge(), 30));
+        System.out.println(android(generateRandomAge(), 4));
     }
     public static String android(int ageHuman, int outdoorTemperature) {
-        if (ageHuman >= generateRandomAge() && ageHuman <= generateRandomAge() && outdoorTemperature >= -30 && outdoorTemperature <= 20) {
+        if (ageHuman >= 20 && ageHuman <= 45 && outdoorTemperature >= -30 && outdoorTemperature <= 20) {
             return "Можно идти гулять!";
         }
-        else if  (ageHuman < generateRandomAge() && outdoorTemperature > 0 && outdoorTemperature < 28) {
+        else if  (ageHuman < 20 && outdoorTemperature > 0 && outdoorTemperature < 28) {
             return "Можно идти гулять!";
-        } else if (ageHuman < generateRandomAge() && outdoorTemperature > -10 && outdoorTemperature < 25) {
+        } else if (ageHuman < 20 && outdoorTemperature > -10 && outdoorTemperature < 25) {
             return "Можно идти гулять!";
         } else {
             return "Оставайтесь дома!";
